3e939dbed76b6b51dbab7e88f9da9966b3a66785
MshNiyaz <niyazshahul2006@gmail.com>
2022-06-24 20:08:36 +0800

    Added models to handle switching between static pages

--- app/models.py
+++ app/models.py
@@ -1,4 +1,67 @@
+import colorsys
+
 # Handle multiple pages
-class PageHandler:
-    def __init__(self):
-        pass
+class PageManager:
+    def __init__(self, pages, sections):
+        for section in sections:
+            # Handle single number section ranges
+            if type(section.range) != type([]):
+                section.range = [section.range, section.range]
+            # Associate pages and sections
+            for i in range(section.range[0], section.range[1] + 1):
+                pages[i - 1].section = section
+                pages[i - 1].index = i
+        self.pages = pages
+        self.sections = sections
+        self.currentPageIndex = 1
+
+    def getCurrentPage(self, currentPageIndex):
+        return self.pages[currentPageIndex]
+
+    def setIndex(self, index):
+        self.currentPageIndex = index
+
+
+# Intiialize page
+class Page:
+    def __init__(self, file, section=None, index=0):
+        self.file = file
+        self.section = section
+        self.index = index
+
+
+# * Color convenience class
+class Color:
+    def __init__(self, hex):
+        self.hex = hex
+        rgb = Color.hex2rgb(hex)
+        self.rgb = rgb
+        self.hsv = colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+
+    def dimmed(self, dimFactor):
+        hsv = self.hsv
+        luminance = hsv[-1]
+        luminance -= dimFactor
+        if luminance < 0:
+            luminance = 0
+        elif luminance > 1:
+            luminance = 1
+        newRgb = colorsys.hsv_to_rgb(hsv[0], hsv[1], luminance)
+        return Color(Color.rgb2hex(newRgb))
+
+    @staticmethod
+    def hex2rgb(h):
+        return tuple(int(h[i : i + 2], 16) / 255.0 for i in (1, 3, 5))  # skip '#'
+
+    @staticmethod
+    def rgb2hex(rgb):
+        return f"#{int(rgb[0]*255):02x}{int(rgb[1]*255):02x}{int(rgb[2]*255):02x}"
+
+
+# Define a section
+class Section:
+    def __init__(self, name, range, theme: Color):
+        self.name = name
+        self.range = range
+        self.theme = theme
+        self.alt_theme = theme.dimmed(dimFactor=0.3)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from werkzeug.exceptions import HTTPException
 
 # Import custom objects
 from app import app
 from app.models import *
+
+# Initialize globals
+pages = [
+    Page("preface.html"),
+    Page("logic_gate_i.html"),
+    Page("logic_gate_II.html"),
+]
+sections = [
+    Section("Preface", [1, 1], Color("#00ff99")),
+    Section("LGates", [2, 3], Color("#00ff99")),
+]
+pageManager = PageManager(pages, sections)
 
 # Title page
 @app.route("/")
@@ -12,13 +24,22 @@ def title():
     return render_template("title.html")
 
 
-@app.route("/<generic>/")
-def show_user_profile(generic):
-    # Show route-based template
-    return f"Generic {generic}"
-
-
 # Catch errors
 @app.errorhandler(HTTPException)
 def handle_exception(e):
     return render_template("error.html", error=e)
+
+
+# Page handler
+@app.route("/tutorial/")
+def show_tutorial_page():
+    file = pageManager.getCurrentPage().file
+    pageIndex = pageManager.currentPageIndex
+    return render_template(pageManager.getCurrentPage().file)
+
+
+# Handle jumps between pages
+@app.route("/tutorial/jumpTo/<index>")
+def jump_to_index(index):
+    pageManager.setIndex(index)
+    return redirect(url_for("show_tutorial_page"))
