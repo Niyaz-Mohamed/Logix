a9b153e752c18096bc4687fa53a5292b6ae406bc
MshNiyaz <niyazshahul2006@gmail.com>
2022-06-24 10:45:42 +0800

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,4 @@
+# Handle multiple pages
+class PageHandler:
+    def __init__(self):
+        pass
---
+++ app/views.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from werkzeug.exceptions import HTTPException
+
+# Import custom objects
+from app import app
+from app.models import *
+
+# Title page
+@app.route("/")
+@app.route("/start")
+def title():
+    return render_template("base.html")
+
+
+@app.route("/<generic>/")
+def show_user_profile(generic):
+    # Show route-based template
+    return f"Generic {generic}"
+
+
+# Catch errors
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    return render_template("error.html", error=e)
